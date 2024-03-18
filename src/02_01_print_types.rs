a701a458a200ee58c1c767ae6856be8033f64852
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-03-18 18:54:51 +0100

    complete 02_01_print_types

--- src/02_01_print_types.rs
+++ src/02_01_print_types.rs
@@ -4,4 +4,10 @@ fn main() {
     print!("{}\n",s);           // \n druckt mit Zeilenumbruch
     println!("{}", s);          // mit Zeilenumbruch
 
+    let name = "Anatoli";
+    let s2 = format!("Hello {}, nice to meet you!", name);
+    println!("{}", s2);
+    // seit Rust 1.58 auch:
+    let s3 = format!("Hello {name}, nice to meet you!");
+    println!("{}", s3);
 }
