02b1e3b307aece74b5eae4737354e3e8fc09e1ef
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-03-18 18:45:09 +0100

    add 02_01_print_types

---
+++ src/02_01_print_types.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let s = "Hello World";
+    print!("{}",s);             // print druckt ohne Zeilenumbruch
+    print!("{}\n",s);           // \n druckt mit Zeilenumbruch
+    println!("{}", s);          // mit Zeilenumbruch
+
+}
