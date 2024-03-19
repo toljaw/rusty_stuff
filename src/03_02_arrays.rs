f837ce4a4545c8c793244950eaee683046f39f6d
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-03-19 17:01:41 +0100

    add and complete 03_02_arrays

--- src/03_02_arrays.rs
+++ src/03_02_arrays.rs
@@ -1,1 +1,20 @@
-fn main() {}
+// Arrays: Fixed length, only same data types
+
+fn main() {
+    let _array = [1, 2, 3];
+
+    println!("{_array:?}");
+    println!("{_array:#?}");        // # for pretty printing
+    println!("{}", _array[0]);      // output single array content
+    println!("{}", _array[1]);
+    println!("{}", _array[2]);
+
+    let _array2: [i32; 5] = [1337; 5];
+
+    println!("{:?}", _array2);
+    println!("{}", _array2[0]);
+    println!("{}", _array2[1]);
+    println!("{}", _array2[2]);
+    println!("{}", _array2[3]);
+    // println!("{}", _array2[5]);      // 5 is out of bounce - compiler will also complain
+}
